a84cd6ca534f1729947028d9ae3969e48b67fc90
Marcus Sonestedt <marcus.lindblom.sonestedt@bitaddict.se>
2019-12-03 11:19:23 +0100

    Allow null files on activity type

--- app/models.py
+++ app/models.py
@@ -128,7 +128,7 @@ class ActivityType(models.Model):
     name = models.CharField(max_length=40, unique=True)
     description = models.TextField(blank=True)
     image = models.ImageField(null=True, blank=True)
-    files = models.ManyToManyField(Attachment)
+    files = models.ManyToManyField(Attachment, blank=True)
 
     def __str__(self):
         return self.name
