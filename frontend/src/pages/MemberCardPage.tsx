75d0b765d71b32b6f404099a6eb6f1eb53a8cf12
Marcus Sonestedt <marcus.s.lindblom@gmail.com>
2020-02-06 09:41:00 +0100

    Add pagination and tooltip to membercard page

--- frontend/src/pages/MemberCardPage.tsx
+++ frontend/src/pages/MemberCardPage.tsx
@@ -1,10 +1,10 @@
-import { Tab, Row, Col, Nav, Table, Button, Container } from 'react-bootstrap';
-import React, { useState, useContext } from "react";
+import { Tab, Row, Col, Nav, Table, Button, Container, Pagination } from 'react-bootstrap';
+import React, { useState, useContext, useMemo } from "react";
 import { deserialize } from "class-transformer";
 
 import { Member, PagedMembers } from '../Models';
 import DataProvider from "../components/DataProvider";
-import { ErrorBoundary, InfoText } from "../components/Utilities";
+import { ErrorBoundary, InfoText, PageItems, HoverTooltip } from "../components/Utilities";
 import { useHistory } from "react-router-dom";
 import { userContext } from '../components/UserContext';
 import { getJsonHeaders } from '../logic/ADRActions';
@@ -58,6 +58,7 @@ export const MemberCardPage = () => {
 
 const ReadyTab = () => {
     const [members, setMembers] = useState(new PagedMembers())
+    const [page, setPage] = useState(1)
 
     const handoutCard = (member: Member) => {
         var number = prompt('Ange guldkortsnummer');
@@ -74,22 +75,37 @@ const ReadyTab = () => {
             .finally(() => window.location.reload())
     }
 
-    const renderRow = (member: Member) =>
-        <tr key={member.id}>
-            <td><a href={member.url()}>{member.fullname}</a></td>
-            <td><a href={`mailto:${member.email}`}>{member.email}</a></td>
-            <td><a href={`tel:${member.phone_number}`}>{member.phone_number}</a></td>
-            <td>{member.booked_weight_year}</td>
-            <td><Button onClick={() => handoutCard(member)} variant='primary'>Lämna ut guldkort</Button></td>
-            <td><Button href={member.adminUrl()} variant='secondary'>Administrera</Button></td>
-        </tr>
+    const pageSize = 10;
+
+    const rows = useMemo(() => {
+        const renderRow = (member: Member) =>
+            <tr key={member.id}>
+                <td><a href={member.url()}>{member.fullname}</a></td>
+                <td><a href={`mailto:${member.email}`}>{member.email}</a></td>
+                <td><a href={`tel:${member.phone_number}`}>{member.phone_number}</a></td>
+                <td>{member.booked_weight_year}</td>
+                <td><Button onClick={() => handoutCard(member)} variant='primary'>Lämna ut guldkort</Button></td>
+                <td><Button href={member.adminUrl()} variant='secondary'>Administrera</Button></td>
+            </tr>
+
+        return members.results
+            .slice((page - 1) * pageSize, page * pageSize)
+            .map(renderRow)
+    }, [members, page])
 
     return <Container fluid>
         <Row>
             <Col md={9}>
-                <h1>Medlemmar som är redo att hämta ut guldkort</h1>
+                <h1>
+                    <span className="table-title">
+                        Medlemmar som är redo att hämta ut guldkort
+                    </span>
+                    <span className="table-count">
+                        {rows.length} / {members.count}
+                    </span>
+                </h1>
                 <DataProvider<PagedMembers>
-                    url='/api/members/ready'
+                    url={`/api/members/ready`}
                     ctor={json => deserialize(PagedMembers, json)}
                     onLoaded={setMembers}>
                     <Table striped hover>
@@ -104,20 +120,24 @@ const ReadyTab = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {members.results.map(renderRow)}
+                            {rows}
                         </tbody>
                     </Table>
+                    <Pagination>
+                        <PageItems count={members.count} pageSize={pageSize} currentPage={page} setFunc={setPage} />
+                    </Pagination>
                 </DataProvider>
             </Col>
             <Col md={3}>
                 <InfoText textKey="membercard-ready" />
             </Col>
         </Row>
-    </Container>
+    </Container >
 }
 
 const HasCardTab = () => {
     const [members, setMembers] = useState(new PagedMembers())
+    const [page, setPage] = useState(1)
 
     const recallCard = (member: Member) => {
         fetch(member.apiUrl(),
@@ -131,18 +151,34 @@ const HasCardTab = () => {
             .finally(() => window.location.reload())
     }
 
-    const renderRow = (member: Member) =>
-        <tr key={member.id}>
-            <td><a href={member.url()}>{member.fullname}</a></td>
-            <td>{member.membercard_number}</td>
-            <td><Button onClick={() => recallCard(member)} variant='danger'>Återkalla guldkort</Button></td>
-            <td><Button href={member.adminUrl()}>Administrera</Button></td>
-        </tr>
+    const pageSize = 10;
+
+    const rows = useMemo(() => {
+        const renderRow = (member: Member) =>
+            <tr key={member.id}>
+                <td><a href={member.url()}>{member.fullname}</a></td>
+                <td>{member.membercard_number}</td>
+                <td><Button onClick={() => recallCard(member)} variant='danger'>Återkalla guldkort</Button></td>
+                <td><Button href={member.adminUrl()}>Administrera</Button></td>
+            </tr>
+
+        return members.results
+            .slice((page - 1) * pageSize, page * pageSize)
+            .map(renderRow)
+    }, [members, page])
 
     return <Container fluid>
         <Row>
             <Col md={9}>
-                <h1>Medlemmar som har guldkort</h1>
+                <h1>
+                    <span className="table-title">
+                        Medlemmar som har guldkort
+                    </span>
+                    <span className="table-count">
+                        {rows.length} / {members.count}
+                    </span>
+                </h1>
+
                 <DataProvider<PagedMembers>
                     url='/api/members/has_card'
                     ctor={json => deserialize(PagedMembers, json)}
@@ -157,9 +193,12 @@ const HasCardTab = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {members.results.map(renderRow)}
+                            {rows}
                         </tbody>
                     </Table>
+                    <Pagination>
+                        <PageItems count={members.count} pageSize={pageSize} currentPage={page} setFunc={setPage} />
+                    </Pagination>
                 </DataProvider>
             </Col>
             <Col md={3}>
@@ -171,31 +210,54 @@ const HasCardTab = () => {
 
 const NeedTasksTab = () => {
     const [members, setMembers] = useState(new PagedMembers())
+    const [page, setPage] = useState(1)
     const user = useContext(userContext);
+    const pageSize = 10;
+    const rows = useMemo(() => {
+        const renderRow = (member: Member) =>
+            <tr key={member.id}>
+                <td><a href={member.url()}>{member.fullname}</a></td>
+                <td>
+                    {'Email: '}
+                    {member.email_verified ? 'OK ✔' : 'SAKNAS ❌'}
+                    <br />
+                    {'Telefon: '}
+                    {member.phone_verified ? 'OK ✔' : 'SAKNAS ❌'}
+                </td>
+                <td>
+                    <HoverTooltip tooltip={`Skall vara minst ${user.minSignups}`} placement='top'>
+                        <div>
+                            {member.booked_weight_year}
+                            <br />
+                            {member.booked_weight_year
+                                ? member.booked_weight_year >= user.minSignups
+                                    ? 'OK'
+                                    : 'OTILLRÄCKLIGT ❌'
+                                : null}
+                        </div>
+                    </HoverTooltip>
+                </td>
+                <td><Button href={member.adminUrl()} variant='secondary'>Administrera</Button></td>
+            </tr>
 
-    const renderRow = (member: Member) =>
-        <tr key={member.id}>
-            <td><a href={member.url()}>{member.fullname}</a></td>
-            <td>
-                {'Email: '}
-                {member.email_verified ? 'OK ✔' : 'SAKNAS ❌'}
-                <br/>
-                {'Telefon: '}
-                {member.phone_verified ? 'OK ✔' : 'SAKNAS ❌'}
-            </td>
-            <td>{member.booked_weight_year}
-                <br/>
-                {member.booked_weight_year
-                    ? member.booked_weight_year >= user.minSignups ? 'OK' : 'OTILLRÄCKLIGT ❌'
-                    : null}
-            </td>
-            <td><Button href={member.adminUrl()} variant='secondary'>Administrera</Button></td>
-        </tr>
+        return members.results
+            .slice((page - 1) * pageSize, page * pageSize)
+            .map(renderRow)
+    }, [members, page, user])
+
 
     return <Container fluid>
         <Row>
             <Col md={9}>
-                <h1>Medlemmar som bokat minst en aktivitet, men inte är klara för guldkort</h1>
+                <h1>
+                    <span className="table-title">
+                        Medlemmar som inte är klara för guldkort
+                    </span>
+                    <span className="table-count">
+                        {rows.length} / {members.count}
+                    </span>
+                </h1>
+
                 <DataProvider<PagedMembers>
                     url='/api/members/not_ready'
                     ctor={json => deserialize(PagedMembers, json)}
@@ -210,13 +272,15 @@ const NeedTasksTab = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {members.results.map(renderRow)}
+                            {rows}
                         </tbody>
                     </Table>
-
+                    <Pagination>
+                        <PageItems count={members.count} pageSize={pageSize} currentPage={page} setFunc={setPage} />
+                    </Pagination>
                 </DataProvider>
             </Col>
-            <Col  md={3}>
+            <Col md={3}>
                 <InfoText textKey="membercard-needtasks" />
             </Col>
         </Row>
