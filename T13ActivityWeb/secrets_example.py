2434d4b899af00fba4ed348e6f28930a326089ef
Marcus Sonestedt <marcus.s.lindblom@gmail.com>
2020-02-06 12:01:56 +0100

    Add example secrets file

---
+++ T13ActivityWeb/secrets_example.py
@@ -0,0 +1,19 @@
+#RECAPTCHA_PUBLIC_KEY = 'client_key'
+#RECAPTCHA_PRIVATE_KEY = 'site_key'
+
+#TWILIO_ACCOUNT_SID = 'sid'
+#TWILIO_AUTH_TOKEN = 'token'
+#TWILIO_VERIFY_SID = 'sid2'
+
+#SMS_FROM_NUMBER = 'TEAM13 LOCAL'
+
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'firstname.lastname@gmail.com'
+#EMAIL_HOST_PASSWORD = 'verysecret'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+
+#SENDGRID_API_KEY = 'secret'
+
+DEBUG = True
+
