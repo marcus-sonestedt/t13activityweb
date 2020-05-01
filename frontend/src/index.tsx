2ce14e9ad55775a3740d4f4cef8912fede7bb2a0
SPDavid <david@zalosolutions.com>
2020-05-01 07:53:45 +0200

    IE Pollyfills

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import './index.css';
 import {App} from './App';
 import "reflect-metadata"
