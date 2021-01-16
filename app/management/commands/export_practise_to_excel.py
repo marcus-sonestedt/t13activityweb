012085dda64fe370f0798a1f58b7a99a67c93bb0
Marcus Sonestedt <marcus.s.lindblom@gmail.com>
2021-01-16 16:41:17 +0100

    Split rows per different times in export

--- app/management/commands/export_practise_to_excel.py
+++ app/management/commands/export_practise_to_excel.py
@@ -17,27 +17,35 @@ def exportPractiseSessions(file, year):
     pitguide_type = ActivityType.objects.get(name="Depå-fadder")
 
     sheet.cell(1, 1).value = "Datum"
-    sheet.cell(1, 2).value = flaggvakt_type.name
-    sheet.cell(1, 3).value = pitguide_type.name
+    sheet.cell(1, 2).value = "Tid"
+    sheet.cell(1, 3).value = flaggvakt_type.name
+    sheet.cell(1, 4).value = pitguide_type.name
 
-    def activity_assigned(event, type):
-        activities = event.activities.filter(type=type)
+    def activity_assigned(event, start_time, type):
+        activities = event.activities.filter(start_time=start_time, type=type)
         if not activities:
             return ''
 
         return ', '.join((m.assigned.get_fullname() if m.assigned else '') for m in activities)
 
-    row = 3
+    row = 2
     for event in Event.objects.filter(start_date__year=year, type__name="Träning"):
-        sheet.cell(row, 1).value = event.date
-        sheet.cell(row, 2).value = activity_assigned(event, flaggvakt_type)
-        sheet.cell(row, 3).value = activity_assigned(event, pitguide_type)
-        row += 1
+        for a in event.activities.filter(type__in=[flaggvakt_type, pitguide_type]).order_by('start_time'):
+            t = f'{a.start_time} - {a.end_time}'
+            if sheet.cell(row, 2).value != t:
+                row += 1
+                sheet.cell(row, 1).value = event.date
+
+            sheet.cell(row, 2).value = t
+            sheet.cell(row, 3).value = activity_assigned(event, a.start_time, flaggvakt_type)
+            sheet.cell(row, 4).value = activity_assigned(event, a.start_time, pitguide_type)
 
     for cd in sheet.column_dimensions:
         cd.bestFit = True
 
     wb.save(file)
+
+    print(f"Exported {row - 2} practise event(s) for year {year} to {file}.")
 
 class Command(BaseCommand):
     help = 'Export coordinator and support for all practise sessions for a year'
